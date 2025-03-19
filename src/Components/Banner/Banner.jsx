85aa72f99306d56cba7f2180c7d3a28b7918a6f2
MH-liton-360 <liton72524nk@gmail.com>
2025-03-20 05:54:41 +0600

    Banner Done

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,19 +1,25 @@
+import Bannerimg from '../../assets/banner.jpg'
+import './Banner.css'
 
 const Banner = () => {
     return (
         <div>
-            <div className="hero bg-blue-500">
+            <div className="hero bg-blue-500 pb-24">
                 <div className="hero-content text-center">
                     <div className="max-w-5xl text-white">
                         <h1 className="text-5xl font-bold">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
-                        <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        <p className="text-sm max-w-2xl mx-auto py-6">
+                            Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!
                         </p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <button className="btn bg-white rounded-xl text-blue-700">Get Started</button>
                     </div>
                 </div>
             </div>
+
+            <div className='-translate-y-22 justify-items-center'>
+                <img className='extra  rounded-3xl' src={Bannerimg} alt="" />
+            </div>
+
 
         </div>
     );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,9 +4,9 @@ const Navbar = () => {
 
     const links = <>
 
-        <li> <NavLink to={'/'}>Home</NavLink> </li>
-        <li><a>Statistics</a></li>
-        <li><a>Dashboard</a></li>
+        <li className="text-white"> <NavLink to={'/'}>Home</NavLink> </li>
+        <li className="text-white"><a>Statistics</a></li>
+        <li className="text-white"><a>Dashboard</a></li>
     </>
 
 
@@ -24,7 +24,7 @@ const Navbar = () => {
                         {links}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl text-white">Gadget Heaven</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
