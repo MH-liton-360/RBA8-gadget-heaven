5611a017b412619effc40520986943944f73a600
MH-liton-360 <liton72524nk@gmail.com>
2025-03-25 21:20:28 +0600

    Done

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -9,6 +9,7 @@ import { Helmet } from 'react-helmet';
 const Dashboard = () => {
 
     const [cartList, setCartList] = useState([]);
+    const [sort, setSort] = useState('');
 
     const allProducts = useLoaderData();
 
@@ -22,7 +23,23 @@ const Dashboard = () => {
 
         setCartList(readProductList);
 
-    }, [allProducts])
+    }, [allProducts]);
+
+    const handleSort = sortType => {
+        setSort(sortType);
+
+        if (sortType === 'Ratings') {
+            const storedCartList = [...cartList].sort((a, b) => b.rating - a.rating);
+            setCartList(storedCartList);
+
+        }
+        if (sortType === 'Price') {
+            const storedCartList = [...cartList].sort((a, b) => b.price - a.price);
+            setCartList(storedCartList);
+
+        }
+    }
+
 
     return (
         <div className="pt-5">
@@ -36,6 +53,24 @@ const Dashboard = () => {
                 <h2 className="text-3xl font-bold">Product Details</h2>
                 <p className="text-sm max-w-2xl mx-auto p-2">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
 
+            </div>
+
+
+            <div className="dropdown dropdown-bottom pt-7 justify-items-end">
+                <div tabIndex={0} role="button" className="btn m-1">
+                    {
+                        sort ? `Sort by: ${sort}` : 'Sort By ⬇️'
+                    }
+                </div>
+                <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                    <li onClick={() => handleSort('Ratings')}><a>Ratings</a></li>
+                    <li onClick={() => handleSort('Price')}><a>Price</a></li>
+                </ul>
+                <button className="btn btn-sm btn-outline btn-primary rounded-xl">Purchase</button>
+
+            </div>
+
+            <div className='pt-20'>
                 <Tabs>
                     <TabList>
                         <Tab>Cart</Tab>
@@ -48,11 +83,15 @@ const Dashboard = () => {
                         <div className="pt-5 gap-4 min-h-96">
                             {
                                 cartList.map(product => (
-                                    <div className='border-4 border-amber-300' key={product.product_id}>
+                                    <div className='flex gap-5 py-2 border-b border-amber-100' key={product.product_id}>
+
                                         <img src={product.product_image} alt={product.title} className="h-16" />
-                                        <h3 className="text-lg font-bold mt-2">{product.product_title}</h3>
-                                        <p className="text-gray-600">⭐ {product.rating} / 5</p>
-                                        <p className="text-blue-500 font-bold">${product.price}</p>
+                                        <div>
+                                            <h3 className="text-lg font-semibold mt-2">{product.product_title}</h3>
+                                            <p className="text-gray-600">⭐ {product.rating} / 5</p>
+                                            <p className="text-blue-500 font-bold">${product.price}</p>
+                                        </div>
+
                                     </div>
                                 ))
                             }
@@ -64,9 +103,7 @@ const Dashboard = () => {
                 </Tabs>
             </div>
 
-            <div>
-                <h2 className="py-3 font-bold h-96"></h2>
-            </div>
+
 
 
 
